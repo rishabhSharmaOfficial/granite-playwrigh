6768a9039c5d76293cb265a7c0b89237ca40a3d3
Rishabh Sharma <risharma654@gmail.com>
2023-12-19 00:12:53 +0530

    Added test steps

--- playwright-tests/e2e/tests/comments.spec.ts
+++ playwright-tests/e2e/tests/comments.spec.ts
@@ -16,54 +16,73 @@ test.describe("Comments feature", () => {
   });
 
   test("should add a new comment as a creator of task", async({page, taskPage, browser}) => {
-    await page.goto("/");
+    await test.step("Step 1: Go to home page", async () => {
+      await page.goto("/");
+    })
 
-    await taskPage.createTaskAndVerify({ taskName, userName: "Sam Smith" });
-    await taskPage.createCommentAndVerify({page, taskName, comment});
+    await test.step("Step 2: Create task and verify it", async () => {
+      await taskPage.createTaskAndVerify({ taskName, userName: "Sam Smith" });
+    })
 
-    const newUserContext = await browser.newContext({
-      storageState: { cookies: [], origins: [] },
-    });
-    const newUserPage = await newUserContext.newPage();
-    const loginPage = new LoginPage(newUserPage);
+    await test.step("Step 3: Add comment to the task and verify it", async () => {
+      await taskPage.createCommentAndVerify({page, taskName, comment});
+    })
 
-    await newUserPage.goto("/");
-    await loginPage.loginAndVerifyUser({
-      email: "sam@example.com",
-      password: "welcome",
-      username: "Sam Smith",
-    });
-    await expect(newUserPage.getByTestId("tasks-pending-table")
-      .getByRole('row', { name: taskName })
-      .getByRole('cell').nth(3)).toHaveText("1");
-    await newUserPage.close();
-    await newUserContext.close();
+    await test.step("Step 4: Login as assignee and verify comment count", async () => {
+      const newUserContext = await browser.newContext({
+        storageState: { cookies: [], origins: [] },
+      });
+
+      const newUserPage = await newUserContext.newPage();
+      const loginPage = new LoginPage(newUserPage);
+
+      await newUserPage.goto("/");
+      await loginPage.loginAndVerifyUser({
+        email: "sam@example.com",
+        password: "welcome",
+        username: "Sam Smith",
+      });
+      await expect(newUserPage.getByTestId("tasks-pending-table")
+        .getByRole('row', { name: taskName })
+        .getByRole('cell').nth(3)).toHaveText("1");
+      await newUserPage.close();
+      await newUserContext.close();
+    })
   })
 
   test("should add a new comment as an assignee of a task", async({page, taskPage, browser}) => {
-    await page.goto("/");
+    await test.step("Step 1: Go to home page", async () => {
+      await page.goto("/");
+    })
 
-    await taskPage.createTaskAndVerify({ taskName, userName: "Sam Smith" });
-    const newUserContext = await browser.newContext({
-      storageState: { cookies: [], origins: [] },
-    });
-    const newUserPage = await newUserContext.newPage();
-    const loginPage = new LoginPage(newUserPage);
-    const newTaskPage = new TaskPage(newUserPage);
+    await test.step("Step 2: Create task and verify it", async () => {
+      await taskPage.createTaskAndVerify({ taskName, userName: "Sam Smith" });
+    })
 
-    await newUserPage.goto("/");
-    await loginPage.loginAndVerifyUser({
-      email: "sam@example.com",
-      password: "welcome",
-      username: "Sam Smith",
-    });
-    await newTaskPage.createCommentAndVerify({taskName, comment});
-    await newUserPage.close();
-    await newUserContext.close();
+    await test.step("Step 3: Login as assignee, add comment to task and verify it", async () => {
+      const newUserContext = await browser.newContext({
+        storageState: { cookies: [], origins: [] },
+      });
+      const newUserPage = await newUserContext.newPage();
+      const loginPage = new LoginPage(newUserPage);
+      const newTaskPage = new TaskPage(newUserPage);
 
-    await page.reload();
-    await expect(page.getByTestId("tasks-pending-table")
+      await newUserPage.goto("/");
+      await loginPage.loginAndVerifyUser({
+        email: "sam@example.com",
+        password: "welcome",
+        username: "Sam Smith",
+      });
+      await newTaskPage.createCommentAndVerify({taskName, comment});
+      await newUserPage.close();
+      await newUserContext.close();
+    })
+
+    await test.step("Step 4: Verify comment count as a creator of the task", async () => {
+      await page.reload();
+      await expect(page.getByTestId("tasks-pending-table")
       .getByRole('row', { name: taskName })
       .getByRole('cell').nth(3)).toHaveText("1");
+    })
   })
 })
